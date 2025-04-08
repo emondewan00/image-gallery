d1d401628f038c2418c274dc203607e215b90e76
emondewan00 <dewanmohammademon@gmail.com>
2025-04-08 22:27:42 +0600

    setup with masonry layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,119 @@
-import Image from "next/image";
+"use client";
+import { Box, Button, Modal } from "@mui/material";
+import React, { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-export default function Home() {
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  // border: "1px solid black",
+  p: 8,
+};
+
+const Home = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [active, setActive] = useState<{ img: string; title: string }>();
+  const handleClose = () => setOpen(false);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="container mx-auto relative">
+      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 py-4 gap-4">
+        {itemData.map((item, i) => (
+          <div key={i} className="">
+            <Button
+              onClick={() => {
+                setOpen(true);
+                setActive(item);
+              }}
+            >
+              <img
+                src={item.img}
+                alt={item.title}
+                className="w-full object-contain rounded-lg"
+              />
+            </Button>
+          </div>
+        ))}
+      </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <>
+          <div className="flex justify-end mr-10 mt-10 gap-4 ">
+            <button className=" cursor-pointer" onClick={handleClose}>
+              <CloseIcon className="text-white" />
+            </button>
+            <button className="bg-red-400 text-white px-2 py-1 rounded cursor-pointer">
+              <DeleteIcon />
+            </button>
+          </div>
+          <Box sx={style}>
+            <img
+              className="object-contain size-96  border"
+              src={active?.img}
+              alt={active?.title}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          </Box>
+        </>
+      </Modal>
     </div>
   );
-}
+};
+
+export default Home;
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    title: "Bed",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    title: "Books",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    title: "Sink",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    title: "Kitchen",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    title: "Blinds",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    title: "Chairs",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+    title: "Laptop",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+    title: "Doors",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+    title: "Coffee",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+    title: "Storage",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+    title: "Candle",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+    title: "Coffee table",
+  },
+];
